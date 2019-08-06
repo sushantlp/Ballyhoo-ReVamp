ce245def608d3ee9ad4e8323b504ea7721562c3f
neo <sushantsingh.1081@gmail.com>
2019-08-06 18:01:04 +0530

    Working detail overview ui

--- components/overview.js
+++ components/overview.js
@@ -1,36 +1,41 @@
+import { Divider } from "semantic-ui-react";
 import Map from "./map";
 import "./overview.css";
 const Overview = props => {
   return (
     <div className="overview-container">
-      <div className="columns">
-        <div className="column is-7">
-          <h4 className="google partner-title">55 Wall Street</h4>
-        </div>
+      <div className="box">
+        <div className="columns">
+          <div className="column is-6">
+            <h4 className="google partner-title">55 Wall Street</h4>
+          </div>
 
-        <div className="column is-2">
-          <span className="tag radius20 fw7">
-            <img src="https://img.icons8.com/color/17/000000/hearts.png" />
-            <span className="ellipsis pl0_5">71%</span>
-          </span>
-        </div>
+          <div className="column is-3">
+            <span className="tag radius20 fw7">
+              <img src="https://img.icons8.com/color/17/000000/hearts.png" />
+              <span className="ellipsis pl0_5">71%</span>
+            </span>
+          </div>
 
-        <div className="column is-3">
-          <span className="rating">
-            <img src="https://img.icons8.com/color/20/000000/filled-star.png" />
-            <img src="https://img.icons8.com/color/20/000000/filled-star.png" />
-            <img src="https://img.icons8.com/color/20/000000/star-half-empty.png" />
-            <img src="https://img.icons8.com/color/20/000000/star.png" />
-            <img src="https://img.icons8.com/color/20/000000/star.png" />
-          </span>
+          <div className="column is-3">
+            <span className="rating">
+              <img src="https://img.icons8.com/color/20/000000/filled-star.png" />
+              <img src="https://img.icons8.com/color/20/000000/filled-star.png" />
+              <img src="https://img.icons8.com/color/20/000000/star-half-empty.png" />
+              <img src="https://img.icons8.com/color/20/000000/star.png" />
+              <img src="https://img.icons8.com/color/20/000000/star.png" />
+            </span>
+          </div>
         </div>
       </div>
 
+      <hr className="spacer is-3" />
+
       <div className="box">
         <div className="cuisine-container">
-          <h4 className="google cuisine-title">Cuisines</h4>
           <div className="columns">
-            <div className="column">
+            <div className="column is-7">
+              <h4 className="google cuisine-title">Cuisines</h4>
               <ul className="package-tags">
                 <li className="ellipsis">Shikara Ride</li>
                 <li className="ellipsis">Nature</li>
@@ -43,16 +48,25 @@ const Overview = props => {
                 <li className="ellipsis">Budget</li>
               </ul>
             </div>
+
+            <div className="column is-5">
+              <h4 className="google cuisine-title">Timings</h4>
+              <ul className="package-tags">
+                <li className="ellipsis">Tue 11:30 AM To 12:30 AM</li>
+                <li className="ellipsis">Wed 11:30 AM To 12:30 AM</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="columns">
-        <div className="column">
-          <h4 className="ff f10 f1-1 fw2">
-            C. d'Arístides Maillol, 12, 08028 Barcelona, Spain
-          </h4>
-          <Map />
+      <div className="box">
+        <div className="columns">
+          <div className="column">
+            <h4 className="ff f10 f1-1 fw2">
+              C. d'Arístides Maillol, 12, 08028 Barcelona, Spain
+            </h4>
+            <Map />
+          </div>
         </div>
       </div>
     </div>
