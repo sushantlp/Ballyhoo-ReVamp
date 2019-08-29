e679f410dbe31b2403b0d1a62b00d49e30683af9
Elliot <sushantsingh.1081@gmail.com>
2019-08-29 11:43:26 +0530

    Redux devtool integrated

--- redux/index.js
+++ redux/index.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers';
 
 
 export const initStore = (initialState = {}) => {
-  return createStore(reducer, initialState, applyMiddleware(thunk));
+  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
 };
