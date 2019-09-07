f4f8d97709d869f8d8420d33a7af5e7eb4b4c107
Elliot <sushantsingh.1081@gmail.com>
2019-09-07 14:58:14 +0530

    Popular component finish

--- components/popular.js
+++ components/popular.js
@@ -4,12 +4,26 @@ export default class Popular extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hover: false
+      hover1: false,
+      hover2: false,
+      hover3: false,
+      hover4: false,
+      hover5: false,
+      hover6: false,
+      hover7: false,
+      hover8: false
     };
   }
 
-  toggleHover = () => {
-    this.setState({ hover: !this.state.hover });
+  toggleHover = index => {
+    if (index === 1) this.setState({ hover1: !this.state.hover1 });
+    else if (index === 2) this.setState({ hover2: !this.state.hover2 });
+    else if (index === 3) this.setState({ hover3: !this.state.hover3 });
+    else if (index === 4) this.setState({ hover4: !this.state.hover4 });
+    else if (index === 5) this.setState({ hover5: !this.state.hover5 });
+    else if (index === 6) this.setState({ hover6: !this.state.hover6 });
+    else if (index === 7) this.setState({ hover7: !this.state.hover7 });
+    else if (index === 8) this.setState({ hover8: !this.state.hover8 });
   };
 
   render() {
@@ -39,10 +53,14 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[0].img})`
                 }}
-                onMouseEnter={this.toggleHover}
-                onMouseLeave={this.toggleHover}
+                onMouseEnter={() => this.toggleHover(1)}
+                onMouseLeave={() => this.toggleHover(1)}
               >
-                <p className="tiletext">{popular[0].title}</p>
+                <p
+                  className={this.state.hover1 ? "tiletext-hover" : "tiletext"}
+                >
+                  {popular[0].title}
+                </p>
               </div>
 
               <div
@@ -50,8 +68,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[1].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(2)}
+                onMouseLeave={() => this.toggleHover(2)}
               >
-                <p className="tiletext" style={{ left: "10vh" }}>
+                <p
+                  className={this.state.hover2 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "10vh" }}
+                >
                   {popular[1].title}
                 </p>
               </div>
@@ -61,8 +84,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[2].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(3)}
+                onMouseLeave={() => this.toggleHover(3)}
               >
-                <p className="tiletext" style={{ left: "10vh" }}>
+                <p
+                  className={this.state.hover3 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "10vh" }}
+                >
                   {popular[2].title}
                 </p>
               </div>
@@ -72,8 +100,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[3].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(4)}
+                onMouseLeave={() => this.toggleHover(4)}
               >
-                <p className="tiletext" style={{ left: "20vh" }}>
+                <p
+                  className={this.state.hover4 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "20vh" }}
+                >
                   {popular[3].title}
                 </p>
               </div>
@@ -85,8 +118,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[4].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(5)}
+                onMouseLeave={() => this.toggleHover(5)}
               >
-                <p className="tiletext" style={{ left: "10vh" }}>
+                <p
+                  className={this.state.hover5 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "10vh" }}
+                >
                   {popular[4].title}
                 </p>
               </div>
@@ -96,8 +134,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[5].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(6)}
+                onMouseLeave={() => this.toggleHover(6)}
               >
-                <p className="tiletext" style={{ left: "10vh" }}>
+                <p
+                  className={this.state.hover6 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "10vh" }}
+                >
                   {popular[5].title}
                 </p>
               </div>
@@ -107,8 +150,13 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[6].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(7)}
+                onMouseLeave={() => this.toggleHover(7)}
               >
-                <p className="tiletext" style={{ left: "20vh" }}>
+                <p
+                  className={this.state.hover7 ? "tiletext-hover" : "tiletext"}
+                  style={{ left: "20vh" }}
+                >
                   {popular[6].title}
                 </p>
               </div>
@@ -117,8 +165,14 @@ export default class Popular extends React.Component {
                 style={{
                   backgroundImage: `url(${popular[7].img})`
                 }}
+                onMouseEnter={() => this.toggleHover(8)}
+                onMouseLeave={() => this.toggleHover(8)}
               >
-                <p className="tiletext">{popular[7].title}</p>
+                <p
+                  className={this.state.hover8 ? "tiletext-hover" : "tiletext"}
+                >
+                  {popular[7].title}
+                </p>
               </div>
             </div>
             <br />
