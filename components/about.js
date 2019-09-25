fa26ef3dd6bf87208b5bcab54a856a1b54fa61a5
sushantlp <sushantsingh.1081@gmail.com>
2019-09-25 15:18:38 +0530

    Working homescreen ui

--- components/about.js
+++ components/about.js
@@ -9,71 +9,100 @@ const AboutComponent = () => (
         </div>
 
         <h4 className="ffqs fw2 mt1 lh1_7">
-          Discover. Eat. Trend. Escape. Ballyhoo Today is a recommendation app
-          to help you discover curated premium Urban Experiences in your city.
-          We understand that you like to explore best of the best hence, we have
-          sorted only the popular ones to choose from - Travel & Leisure,
-          Events, Activities, Restaurants, Gastropub, Breweries, Nightlife Club
-          Parties, Spa and much more. Explore only the best happening places
-          with reviews, popularities, photos, prices and more all at once. Find
-          some thing new everyday, be it - Club Parties (Karaoke Night, Ladies
-          Night, DJ Nights, Bollywood Night, Bhangra Night, Comedy Nights or
-          something else), Fine Dinning or Gastropub. Discover adventure
-          activities, Urban escapes or hidden jewels in your city for exclusive
-          eventful experiences. We provide the top most options for you explore
-          before you decide to step out for leisure. We also provide a
-          convenience of customisation (Escape package), send your query, online
-          reservation & purchase to keep things sorted for you. Enjoy exclusive
-          cashbacks and offers for all advance payments purchases for Ballyhoo
-          Exclusive Packages. We also offer an option to pay at venue with a
-          single click of a fingertip. ✓ Search from a pool of curated premium
-          experiences of your city. Be it Bangalore, Mumbai, Delhi, NCR,
-          Gurgaon, Noida, Pune, Chennai, Hyderabad, Kolkata or Chandigarh
-          Ballyhoo brings all the curated list at your finger tips. We are
-          present in more than 10 cities in India, you can discover just about
-          anything new exciting to do around you. If you are new to a city for a
-          vacation or for business and wonder what to do in that city, simply
-          search for that city and preferable locality. ✓ No more boring
-          weekends. Get updates via push notifications for top trending things
-          to do in your city and other outdoor activities near you. Discover,
-          Reserve, Purchase and Save all together before you even step out. ✓
-          RSVP or reserve for free. Share it with your friends on Facebook with
-          just the click of a button right from the app. Other sharing options
-          like WhatsApp, Twitter sharing also available. ✓ The different types
-          of categories we have for you are as below: - Escape (Travel &
-          Leisure) - Ballyhoo Exclusive Travel packages available with
-          customised destination as you prefer under budget or no budget
-          constraint. Send query or Book Day out & Weekend escape packages from
-          our Verified partners. - Adventurous Activities - from a quick outdoor
-          adrenalin rush to a indoor games facility, we have covered it all - Go
-          karting, laser tag, paintball, bowling, wall climbing, Escape room,
-          Snow activities, camping, Trekking & hiking, quad biking, bull ride,
-          simulator & games. - Club Parties – From House Parties to Club Nights
-          - Ladies Nights, Live Screening, Karaoke Nights, DJ Nights, Bollywood
-          Night, Bhangra Night, Comedy Nights etc. - Events – Be it bollywood,
-          rock, jazz, blues or metal, live performances or Comedy Shows, we have
-          it all. - Fine Dine – Breakfast Buffets, Lunch Buffets, Dinner
-          Buffets, Midnight Buffets, 5 Course Set Menu, 7 Course Buffets and
-          more. - Gastropubs & Breweries – Table Reservations, Curated Packages:
-          Unlimited Beer, Unlimited Drinks, Unlimited Cocktails, Happy Hours. -
-          Dance – EDM, Contemporary Dance, Hip Hop Face Off and much more. -
-          Others - Theatre plays, Group Activities, Pocket Friendly Munching,
-          Stand-up Comedy, Spa and more. ✓ Ballyhoo recommends a personalised
-          list for you at the location of your choice. Also, find trending
-          things-to-do for the weekend, weekdays or any major holidays in
-          advance. Apart from getting an awesome offer we also enrol you for
-          rewards loyalty points to gain more. Our reward program is one of the
-          uniquely designed for you to explore and have a delightful memory. Get
-          rewarded for every purchase or reservation @Ballyhoo and save on
-          future experiences. We religiously believe in being GREEN hence, no
-          paper redemption. With a single click of button, you can reserve,
-          purchase, redeem and gain great loyalty rewards. We're adding more
-          cities, categories & exclusive packages every month. Stay tuned as
-          Ballyhoo Today is a next generation lifestyle guide for Urban Indians
-          (Delhi NCR, Noida, Gurgaon, Mumbai, Hyderabad, Chennai, Pune and
-          Bangalore) to ensure you #StayBallyhoo Come join hundreds of thousands
-          of happy people, who believes in making every day more eventful and
-          more memorable with Ballyhoo Today, and decide yourself!
+          Discover. Eat. Trend. Escape. <br /> <br />
+          Ballyhoo Today is a recommendation app to help you discover curated
+          premium Urban Experiences in your city. We understand that you like to
+          explore best of the best hence, we have sorted only the popular ones
+          to choose from - Travel & Leisure, Events, Activities, Restaurants,
+          Gastropub, Breweries, Nightlife Club Parties, Spa and much more.
+          <br />
+          <br />
+          Explore only the best happening places with reviews, popularities,
+          photos, prices and more all at once. Find some thing new everyday, be
+          it - Club Parties (Karaoke Night, Ladies Night, DJ Nights, Bollywood
+          Night, Bhangra Night, Comedy Nights or something else), Fine Dinning
+          or Gastropub. Discover adventure activities, Urban escapes or hidden
+          jewels in your city for exclusive eventful experiences.
+          <br />
+          <br />
+          We provide the top most options for you explore before you decide to
+          step out for leisure. We also provide a convenience of customisation
+          (Escape package), send your query, online reservation & purchase to
+          keep things sorted for you. Enjoy exclusive cashbacks and offers for
+          all advance payments purchases for Ballyhoo Exclusive Packages. We
+          also offer an option to pay at venue with a single click of a
+          fingertip.
+          <br />
+          <br />
+          ✓ Search from a pool of curated premium experiences of your city. Be
+          it Bangalore, Mumbai, Delhi, NCR, Gurgaon, Noida, Pune, Chennai,
+          Hyderabad, Kolkata or Chandigarh Ballyhoo brings all the curated list
+          at your finger tips. We are present in more than 10 cities in India,
+          you can discover just about anything new exciting to do around you. If
+          you are new to a city for a vacation or for business and wonder what
+          to do in that city, simply search for that city and preferable
+          locality.
+          <br />
+          ✓ No more boring weekends. Get updates via push notifications for top
+          trending things to do in your city and other outdoor activities near
+          you. Discover, Reserve, Purchase and Save all together before you even
+          step out.
+          <br />
+          ✓ RSVP or reserve for free. Share it with your friends on Facebook
+          with just the click of a button right from the app. Other sharing
+          options like WhatsApp, Twitter sharing also available.
+          <br />
+          ✓ The different types of categories we have for you are as below:
+          <br />
+          - Escape (Travel & Leisure) - Ballyhoo Exclusive Travel packages
+          available with customised destination as you prefer under budget or no
+          budget constraint. Send query or Book Day out & Weekend escape
+          packages from our Verified partners.
+          <br />
+          - Adventurous Activities - from a quick outdoor adrenalin rush to a
+          indoor games facility, we have covered it all - Go karting, laser tag,
+          paintball, bowling, wall climbing, Escape room, Snow activities,
+          camping, Trekking & hiking, quad biking, bull ride, simulator & games.
+          <br />
+          - Club Parties – From House Parties to Club Nights - Ladies Nights,
+          Live Screening, Karaoke Nights, DJ Nights, Bollywood Night, Bhangra
+          Night, Comedy Nights etc.
+          <br />
+          - Events – Be it bollywood, rock, jazz, blues or metal, live
+          performances or Comedy Shows, we have it all.
+          <br />
+          - Fine Dine – Breakfast Buffets, Lunch Buffets, Dinner Buffets,
+          Midnight Buffets, 5 Course Set Menu, 7 Course Buffets and more.
+          <br />
+          - Gastropubs & Breweries – Table Reservations, Curated Packages:
+          Unlimited Beer, Unlimited Drinks, Unlimited Cocktails, Happy Hours.
+          <br />
+          - Dance – EDM, Contemporary Dance, Hip Hop Face Off and much more.
+          <br />
+          - Others - Theatre plays, Group Activities, Pocket Friendly Munching,
+          Stand-up Comedy, Spa and more.
+          <br />
+          <br />✓ Ballyhoo recommends a personalised list for you at the
+          location of your choice. Also, find trending things-to-do for the
+          weekend, weekdays or any major holidays in advance. Apart from getting
+          an awesome offer we also enrol you for rewards loyalty points to gain
+          more. Our reward program is one of the uniquely designed for you to
+          explore and have a delightful memory. Get rewarded for every purchase
+          or reservation @Ballyhoo and save on future experiences.
+          <br />
+          <br />
+          We religiously believe in being GREEN hence, no paper redemption. With
+          a single click of button, you can reserve, purchase, redeem and gain
+          great loyalty rewards. We're adding more cities, categories &
+          exclusive packages every month. Stay tuned as Ballyhoo Today is a next
+          generation lifestyle guide for Urban Indians (Delhi NCR, Noida,
+          Gurgaon, Mumbai, Hyderabad, Chennai, Pune and Bangalore) to ensure you
+          #StayBallyhoo
+          <br />
+          <br />
+          Come join hundreds of thousands of happy people, who believes in
+          making every day more eventful and more memorable with Ballyhoo Today,
+          and decide yourself!
         </h4>
       </div>
     </div>
